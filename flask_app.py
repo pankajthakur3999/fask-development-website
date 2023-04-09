af455089ea5d2b807ee2d78c8a7462c78a7ff3c8
Pankaj <pankajthakur3999@gmail.com>
2023-04-09 18:12:26 +0530

    Added styles using Bootstrap

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+JOBS = [
+    {
+    'id':1,
+    'title':'Business Analyst',
+    'location':'Bengluru, India',
+    'salary':'$120,000'
+    },
+    {
+    'id':2,
+    'title':'Data Analyst',
+    'location':'Remote',
+    'salary':'$120,000'
+    },
+]
+
 @app.route("/")
-def hello_world():
-    return render_template('home.html')
+def app_portfolio():
+    return render_template('home.html',jobs=JOBS)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
